9b65433fba65a96445982efc2b47cdd28c84ac69
sakuramioka <lnaresh2006@gmail.com>
2023-10-23 12:52:55 +0530

    Added Vote Button

--- application.py
+++ application.py
@@ -8,17 +8,24 @@ settings = {
     'image_width' : 180, # Width of candidate image
     'image_height' : 180, # Height of candidate image
     'min_spacing' : 100, # Distance between two candidate images (in pixels)
-    'per_row' : 5 # Number of candidates displayed per row
+    'name_size_px' : 13, # Size of candidate's name (in pixels)
+    'class_size_px' : 11, # Size of candidate's class and section (in pixels)
+    'per_row' : 5, # Number of candidates displayed per row
+    'font' : 'Dubai', # Universal font
+    'vote_button_size' : (100, 35)
 }
 
 themes = {
     'Light' : {
         'background_image': "images//background_light.jpg",
         'text_color_1': "Black",
-        'text_color_2': "Grey20"
+        'text_color_2': "Grey20",
+        'vote_button' : "images\\Vote_Button.png"
     }
 }
- 
+
+# Choose a theme
+selected_theme = 'Light' 
 
 # Read from config
 data = open('config.json','r+')
@@ -53,7 +60,7 @@ screen_height = root.winfo_screenheight()
 screen_width = root.winfo_screenwidth()
 
 # Load and resize the background image to fit the window
-background_image = Image.open("images//background_light.jpg")  # Replace with your image path
+background_image = Image.open(themes[selected_theme]['background_image'])  # Replace with your image path
 background_image = background_image.resize((1920, 1080), Image.Resampling.LANCZOS)
 background_image = ImageTk.PhotoImage(background_image)
 
@@ -64,6 +71,7 @@ canvas.pack()
 # Set the background image on the canvas
 canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
 
+# Format displaying images
 def format_images(start, stop, post, y):
     list_width = (settings['image_width'] + settings['min_spacing'])*((stop - start)-1)
     x = (screen_width - list_width)//2
@@ -76,8 +84,14 @@ def format_images(start, stop, post, y):
         image = ImageTk.PhotoImage(image)
         images.append(image)
         canvas.create_image(x, y, image=image, anchor = tk.CENTER)
-        canvas.create_text(x, (y + settings['image_height']//2+ 20), text=election_dictionary[post][index][1].upper(), font=('Dubai', '12', 'bold'), fill='Black', anchor=tk.CENTER)
-        canvas.create_text(x, (y + settings['image_height']//2+ 40), text=f"{election_dictionary[post][index][2]}-{election_dictionary[post][index][3]}", font=('Dubai', '12', 'bold'), fill='Black', anchor=tk.CENTER)
+        canvas.create_text(x, (y + settings['image_height']//2+ 20), text=election_dictionary[post][index][1].upper(), font=(settings['font'], settings['name_size_px'], 'bold'), fill=themes[selected_theme]['text_color_1'], anchor=tk.CENTER, justify='center')
+        canvas.create_text(x, (y + settings['image_height']//2+ 40), text=f"{election_dictionary[post][index][2]}-{election_dictionary[post][index][3]}", font=(settings['font'], settings['class_size_px'], 'bold'), fill=themes[selected_theme]['text_color_2'], anchor=tk.CENTER, justify='center')
+        vote_button_image = Image.open(themes[selected_theme]['vote_button'])
+        vote_button_image = vote_button_image.resize((100, 35), Image.Resampling.LANCZOS)
+        vote_button_image = ImageTk.PhotoImage(vote_button_image)
+        vote_button = tk.Button(canvas, image=vote_button_image,bg="#f0f0f0", bd=2, highlightthickness= 0)
+        vote_button.image = vote_button_image
+        vote_button.place(x=x, y=y + settings['image_height']//2+ 80, anchor='center')
         x = (x + settings['image_width'] + settings['min_spacing'])
 
 # Display candidate images
