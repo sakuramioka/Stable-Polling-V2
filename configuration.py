6062aae53f09c804ac711d299ab5dab6d9c46d5d
sakuramioka <lnaresh2006@gmail.com>
2023-11-19 10:35:41 +0530

    Applying settings

--- configuration.py
+++ configuration.py
@@ -40,16 +40,37 @@ canvas.pack()
 # Set the background image on the canvas
 canvas.create_image(0, 0, anchor=tk.NW, image=background_image)
 
-# Switch button
-switch_button_img = Image.open("images\\switch.png")
-switch_button_img = switch_button_img.resize((screen_height//16, screen_height//16), Image.Resampling.LANCZOS)
-switch_button_img = ImageTk.PhotoImage(switch_button_img)
-switch_button = tk.Button(canvas, image=switch_button_img, bg="#f0f0f0", bd=0)
-switch_button.image = switch_button_img
-switch_button.place(x=20, y=screen_height//19, anchor=tk.W)
+settings_dict = {}
 
+def apply_settings():
+    new_settings = {}
+    for setting in settings_dict:
+        if type(settings_dict[setting]) != list:
+            new_settings[setting] = settings_dict[setting].get()
+            for property in settings:
+                if setting == property[property_name] and property[data_type] == 'Integer':
+                    if new_settings[setting] == None or new_settings[setting] == '':
+                        new_settings[setting] = property[default]
+                    try:
+                        new_settings[setting] = int(new_settings[setting])
+                    except Exception:
+                        new_settings[setting] = property[default]
+        else:
+            new_settings[setting] = [settings_dict[setting][0].get(), settings_dict[setting][1].get()]
+            for property in settings:
+                if setting == property[property_name]:
+                    if new_settings[setting][0] == None or new_settings[setting][0] == '' or new_settings[setting][1] == None or new_settings[setting][1] == '':
+                        new_settings[setting][0] = property[default][0]
+                        new_settings[setting][1] = property[default][1]
+                    try:
+                        new_settings[setting][0] = int(settings_dict[setting][0])
+                        new_settings[setting][1] = int(settings_dict[setting][1])
+                    except Exception:
+                        new_settings[setting][0] = property[default][0]
+                        new_settings[setting][1] = property[default][1]
 
 def generate_settings():
+    global settings_list
     canvas.create_text(40 + screen_height//16, screen_height//18, anchor=tk.W, text='CONFIGURATION', font=('Dubai', screen_height//14, 'bold'), fill='white')
     pad_min = screen_height//100
     pad_max = pad_min*4
@@ -66,22 +87,32 @@ def generate_settings():
         if setting[number_of_entries] == 1:
             entry = tk.Entry(canvas, font= ('Dubai', font_size - 5, 'bold'), justify='right')
             entry.place(x=x, y=y, width=entry_width, height=font_size+pad_min, anchor=tk.W)
+            settings_dict[setting[property_name]] = entry
             canvas.create_text(x + entry_width + 5, y, text= setting[units], font=('Dubai', font_size - 5, 'bold'), fill='Grey20', anchor=tk.W)
             y = y+font_size + pad_max
         elif setting[number_of_entries] == 2:
             canvas.create_text(x, y, text= 'X:', font=('Dubai', font_size - 5, 'bold'), fill='Black', anchor=tk.W)
-            entry = tk.Entry(canvas, font= ('Dubai', font_size - 5, 'bold'), justify='right')
-            entry.place(x=x + font_size, y=y, width=entry_width//2, height=font_size+10, anchor=tk.W)
+            entry0 = tk.Entry(canvas, font= ('Dubai', font_size - 5, 'bold'), justify='right')
+            entry0.place(x=x + font_size, y=y, width=entry_width//2, height=font_size+10, anchor=tk.W)
             canvas.create_text(x + font_size + entry_width//2 + 5, y, text= setting[units], font=('Dubai', font_size - 5, 'bold'), fill='Grey20', anchor=tk.W)
             x_new = x + font_size + entry_width//2 + 40
             canvas.create_text(x_new, y, text= 'Y:', font=('Dubai', font_size - 5, 'bold'), fill='Black', anchor=tk.W)
-            entry = tk.Entry(canvas, font= ('Dubai', font_size - 5, 'bold'), justify='right')
-            entry.place(x=x_new + font_size, y=y, width=entry_width//2, height=font_size+10, anchor=tk.W)
+            entry1 = tk.Entry(canvas, font= ('Dubai', font_size - 5, 'bold'), justify='right')
+            entry1.place(x=x_new + font_size, y=y, width=entry_width//2, height=font_size+10, anchor=tk.W)
+            settings_dict[setting[property_name]] = [entry0, entry1]
             canvas.create_text(x_new + font_size + entry_width//2 + 5, y, text= setting[units], font=('Dubai', font_size - 5, 'bold'), fill='Grey20', anchor=tk.W)
             y = y+font_size + pad_max
         if y+font_size*3+pad_min*3+5 > screen_height:
             x = x + screen_width//3
             y = screen_height//8
+
+# Switch button
+switch_button_img = Image.open("images\\switch.png")
+switch_button_img = switch_button_img.resize((screen_height//16, screen_height//16), Image.Resampling.LANCZOS)
+switch_button_img = ImageTk.PhotoImage(switch_button_img)
+switch_button = tk.Button(canvas, image=switch_button_img, bg="#f0f0f0", bd=0, command=apply_settings)
+switch_button.image = switch_button_img
+switch_button.place(x=20, y=screen_height//19, anchor=tk.W)
 
 generate_settings()
 
